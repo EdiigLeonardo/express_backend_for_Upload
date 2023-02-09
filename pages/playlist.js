9a3ee7509e679d316e2b0b001c66d2b367b7c7ff
anglagomes <109528613+anglagomes@users.noreply.github.com>
2023-02-09 23:20:39 +0000

    a
    # Conflicts:
    #	index.js

--- pages/playlist.js
+++ pages/playlist.js
@@ -8,7 +8,8 @@ router.get('/', async function (req, res) {
 });
 
 router.get('/:id_playlist', async function (req, res) {
-        const playlist = await query(`SELECT video.titulo as name, video.miniatura as thumbnail, data_publicacao,
+        const playlist = await query(`SELECT video.titulo as name, video.miniatura as thumbnail,
+        data_publicacao, imagem, 
         video.id as video_id, playlist.id as playlist_id FROM \`playlist_video\` 
         INNER JOIN playlist ON playlist.id = playlist_video.playlist
         INNER JOIN video ON video.slug = playlist_video.video
