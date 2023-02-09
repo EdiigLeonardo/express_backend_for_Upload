3ddcfe237d69fda03ab67cf110aa83856c5d289e
EdiigLeonardo <65238422+EdiigLeonardo@users.noreply.github.com>
2023-02-09 16:35:24 +0000

    upload-nodeJs
    push

--- pages/videos.js
+++ pages/videos.js
@@ -3,24 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    videos = await query("SELECT * FROM video");
+    const videos = await query("SELECT id as mid, slug, canal as field_canal," +
+        "url_youtube as field_media_oembed_video, miniatura as user_picture," +
+        "descricao as field_descricao, duracao as field_duracao, categoria as field_categoria," +
+        "data_publicacao as created" +
+        " FROM video");
+    for (let video of videos) {
+        try{
+            const tag = await query("SELECT tag from video_tag where video=?",[video.mid]);
+            //perguntar ao professor;
+            video.field_tags = tag[0].tag;
+        }catch (e) {
+        }
+    }
     res.send(videos);
 });
 
-/*
-{
-        "name": "BBB23: ANALISANDO OS PARTICIPANTES feat. Modi | Foquinha FBI",
-        "field_canal": "10",
-        "field_categorias": "<a href=\"/taxonomy/term/38\" hreflang=\"en\">Informação</a>",
-        "field_descricao": "Finalmente a alienação vem ai, mores! Eu e o André fizemos uma analise e demos nossas apostas do que vai rolar no BBB23. Curtiu? Então dá um like aí, meu anjo! ",
-        "field_duracao": "Médio",
-        "thumbnail__target_id": "/sites/default/files/oembed_thumbnails/2023-01/V6tf8lCC2sPPtOo-LFoMuDfLYdTDzjouZ8Go7ZakSs8.jpg",
-        "field_media_oembed_video": "https://www.youtube.com/watch?v=gcgzZIJQ5Gs",
-        "field_tags": "fofoca, informação, famosos",
-        "mid": "108",
-        "created": "1 week 5 days ago",
-        "user_picture": "/sites/default/files/pictures/2022-12/Foquinha.jpg"
-    }
- */
+
 
 module.exports = router;
