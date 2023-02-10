52d216f32ac4e3edda68bd7ba4018203d3e0a914
LuisMartins71 <109528943+LuisMartins71@users.noreply.github.com>
2023-02-10 12:27:28 +0000

    10Fev(1)

--- others/reacao.js
+++ others/reacao.js
@@ -2,30 +2,48 @@ const {query} = require("../connection");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', async function (req, res) {
-    let tematicas = await query(
-        "SELECT id as nid, " +
-        "slug as slug, " +
-        "nome as title, " +
-        "descricao as field_descricao_, " +
-        "imagem as field_imagem," +
-        "capa as field_foto_de_capa " +
-        "FROM `tematica`;"
-    );
-    res.send(tematicas);
+
+router.get('/:id_video', async function (req, res){
+    let likes  = await query("SELECT COUNT (*) FROM reacao WHERE video = ? AND tipo ='like' as Contador_de_likes,"
+        , [req.params.id_video]);
+    res.send(likes);
 });
 
-router.post('/:tipo/:id_video', async function (req, res) {
-    let tematica = await query(
-        "SELECT id as nid, " +
-        "slug as slug, " +
-        "nome as title, " +
-        "descricao as field_descricao_, " +
-        "imagem as field_image," +
-        "capa as field_foto_de_capa " +
-        "FROM `tematica` WHERE id=?;",[req.params.id_tematica]
-    );
-    res.send(tematica);
+router.get('/:id_video', async function (req, res){
+    let dislikes  = await query("SELECT COUNT (*) FROM reacao WHERE video = ? AND tipo ='dislike' as count"
+        , [req.params.id_video]);
+    res.send(dislikes);
 });
 
 module.exports = router;
+
+
+/*router.get('/', async function (req, res) {
+    let reagir = await query("SELECT r.nome, r.tipo, r.data, r.video " +
+        "FROM reacao r INNER JOIN video v ON r.video = v.id WHERE v.id =?", [req.params.id_video]);
+    res.json(reagir);
+});*/
+
+
+
+/*Apenas para listagem de reações
+router.get('/', async function (req, res) {
+    let reacoes = await query(
+        "SELECT id , " +
+        "video as entity_id, " +
+        "tipo, " +
+        "data " +
+        "FROM `reacao`;"
+    );
+    res.send(reacoes);
+});
+
+
+router.post('/:tipo/:id_video', async function (req, res) {
+    let reacao = await query(
+        "INSERT INTO `reacao` (`id`, `video`, `tipo`, `data`) VALUES (NULL, ?, ?, current_timestamp());"
+    );
+    res.send(reacao);
+});*/
+
+
--- others/tags.js
+++ others/tags.js
@@ -14,6 +14,4 @@ router.get('/:tag', async function (req, res) {
 
 });
 
-
-
 module.exports = router;
